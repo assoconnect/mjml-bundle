dcbfec066789369d295f4a593b8c228a50e527b2
Sylvain <syl.fabre@gmail.com>
2019-04-23 11:49:49 +0200

    auto tagging

--- src/DependencyInjection/AssoConnectMJMLExtension.php
+++ src/DependencyInjection/AssoConnectMJMLExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Assoconnect\MJMLBundle\DependencyInjection;
 
+use Assoconnect\MJMLBundle\Tag\TagInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
@@ -19,5 +20,10 @@ class AssoconnectMJMLExtension extends Extension
             new FileLocator(__DIR__ . '/../Resources/config')
         );
         $loader->load('services.yaml');
+
+        // all classes implementing TagInterface will be tagged with assoconnect_mjml.custom_tag
+        // so the bundle user does not need to tag these
+        $container->registerForAutoconfiguration(TagInterface::class)
+            ->addTag('assoconnect_mjml.custom_tag');
     }
 }
